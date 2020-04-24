de2298e7d80ddb703a2499ec50dcbbc26f4f46a4
Foxbuka <krushenitskii@gmail.com>
2020-04-24 16:49:44 +0300

    some deploy troubles

--- src/api/api.js
+++ src/api/api.js
@@ -3,16 +3,17 @@ import * as axios from 'axios';
 // http://api.dev.cakeiteasy.no/api/store/images/4787/?size=small_url&compress_type=webp
 
 const instance = axios.create({
+  withCredentials: true,
   baseURL: "http://api.dev.cakeiteasy.no/api/store/",
 })
 
 
 export const mainApi = {
   getLocations: () => {
-     return instance.get("http://api.dev.cakeiteasy.no/api/store/cities/?country_code=no&most_popular=true")
+     return instance.get("cities/?country_code=no&most_popular=true")
      .then(locations => locations.data)
   },
   getBakeries: () => {
-    return instance.get("http://api.dev.cakeiteasy.no/api/store/bakeries/?country_code=no")
+    return instance.get("bakeries/?country_code=no")
     .then(response => response.data);
   }}
