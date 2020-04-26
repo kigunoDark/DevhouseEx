3c5f15530b37c5338b5f9068d9028b1fe402fa17
Foxbuka <krushenitskii@gmail.com>
2020-04-26 12:56:05 +0300

    with build

---
+++ build/precache-manifest.88d382eab8b0b1489fb6f24ce0f6c20e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc34a6bfc85b56fd23a631a19be3c2c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eac18542fbe8725c4e1a",
+    "url": "/static/css/main.e9ced953.chunk.css"
+  },
+  {
+    "revision": "b19db80c79dac1b970e8",
+    "url": "/static/js/2.3a42ab1d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3a42ab1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac18542fbe8725c4e1a",
+    "url": "/static/js/main.8c73cb44.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  },
+  {
+    "revision": "df2efafe9c15ec36d97c75ef4ffa5bf4",
+    "url": "/static/media/cake2.df2efafe.png"
+  },
+  {
+    "revision": "b25746eacd86ec1f3af0ff22604df746",
+    "url": "/static/media/coffee.b25746ea.jpg"
+  },
+  {
+    "revision": "994c84a2e85c11044725efb67ad8144c",
+    "url": "/static/media/lollipop.994c84a2.jpg"
+  }
+]);
